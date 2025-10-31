6bb5c4dd811983ddbd189f7f3855822b5b64dd5a
GwerhDev <gwerh.dev@gmail.com>
2025-10-31 14:55:35 -0300

    Refactor app.js: streamline middleware setup and update CORS headers

--- src/app.js
+++ src/app.js
@@ -4,16 +4,13 @@ const routes = require("./routes");
 
 const morgan = require("morgan");
 const session = require("express-session");
-
 const passport = require("passport");
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { privateSecret, allowedOrigins } = require("./config");
-const { production } = require("./misc/consts");
 
-server.use(bodyParser.json({ limit: '100mb' }));
-server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-server.use(morgan('combined'));
+const { privateSecret, allowedOrigins, environment } = require("./config");
+const { production } = require("./misc/consts");
 
 server.use((req, res, next) => {
   const origin = req.headers.origin;
@@ -24,31 +21,32 @@ server.use((req, res, next) => {
 
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
+    return res.sendStatus(200);
   }
+
+  next();
 });
 
+server.use(bodyParser.json({ limit: '100mb' }));
+server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 server.use(cookieParser());
+server.use(morgan('dev'));
 
 server.use(session({
   secret: privateSecret,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    sameSite: process.env.NODE_ENV === production ? 'None' : 'Lax',
-    secure: process.env.NODE_ENV === production ? true : false,
-    domain: process.env.NODE_ENV === production ? '.shadowclan.cl' : undefined
+    sameSite: environment === production ? 'None' : 'Lax',
+    secure: environment === production ? true : false,
   }
 }));
-
 server.use(passport.initialize());
-server.use(bodyParser.json());
 server.use(passport.session());
+
 server.use('/', routes);
 
 module.exports = server;
